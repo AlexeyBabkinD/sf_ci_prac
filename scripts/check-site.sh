405a4f57d303ad651d586a8271a8fb8083d2193e
Alexey Babkin <alexey.babkin.d@gmail.com>
2022-12-10 20:58:11 +0300

    fix bash

--- scripts/check-site.sh
+++ scripts/check-site.sh
@@ -6,8 +6,8 @@ echo "Run docker container"
 docker run --name check-site -p 9889:80 my_nginx > /dev/null 2>&1 &
 sleep 5
 echo "Get md5sum index.html in container"
-index_docker = $(docker exec check-site md5sum /usr/share/nginx/html/index.html)
-index_docker = $(echo $index_docker | awk '{ print $1 }')
+index_docker=$(docker exec check-site md5sum /usr/share/nginx/html/index.html)
+index_docker=$(echo $index_docker | awk '{ print $1 }')
 echo "Remove docker"
 docker stop check-site && docker rm check-site && docker rmi my_nginx -f
 
