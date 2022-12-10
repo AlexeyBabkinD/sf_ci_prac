8ce8b12a62cb8ea535482c0d6255d000cee6f911
Alexey Babkin <alexey.babkin.d@gmail.com>
2022-12-10 21:21:18 +0300

    added code check

--- scripts/check-site.sh
+++ scripts/check-site.sh
@@ -8,8 +8,13 @@ docker run --name check-site -p 9889:80 my_nginx > /dev/null 2>&1 &
 sleep 5
 
 echo "Check curl status"
-resp_status=$(curl -I http://158.160.44.195:9889 >/dev/null | head -n 1 | cut -d$' ' -f2)
-echo "$resp_status"
+resp_status=$(curl -s -o /dev/null -I -w "%{http_code}" http://158.160.44.195:9889)
+if [ $resp_status != "200" ]; then
+	echo "Bad response code [$resp_code]"
+	exit 1
+else
+	echo "Response code is $resp_code"
+fi
 
 echo "Get md5sum index.html in container"
 index_docker=$(docker exec check-site md5sum /usr/share/nginx/html/index.html)
