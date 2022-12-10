3e11858806de5915eb6c5c5f3fff068e4f7c19d4
Alexey Babkin <alexey.babkin.d@gmail.com>
2022-12-10 20:45:35 +0300

    fixed code

--- jenkins/check-site.groovy
+++ jenkins/check-site.groovy
@@ -7,8 +7,8 @@ pipeline {
                     sh 'docker build . -t my_nginx'
                     sh 'docker run --name check-site -p 9889:80 my_nginx &'
                     sh 'sleep 5'
-                    sh 'docker exec -it check-site md5sum /usr/share/nginx/html/index.html'
-                    sh 'docker stop check-site && docker rmi my_nginx -f'
+                    sh 'docker exec check-site md5sum /usr/share/nginx/html/index.html'
+                    sh 'docker stop check-site && docker rm check-site && docker rmi my_nginx -f'
                 }
             }
         }
