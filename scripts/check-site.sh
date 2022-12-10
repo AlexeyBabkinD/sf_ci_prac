d4459b3f79f0326a00a5c187da4ca7f4c8994bba
Alexey Babkin <alexey.babkin.d@gmail.com>
2022-12-10 21:11:45 +0300

    add curl check

--- scripts/check-site.sh
+++ scripts/check-site.sh
@@ -2,18 +2,32 @@
 
 echo "Build docker image"
 docker build . -t my_nginx > /dev/null
+
 echo "Run docker container"
 docker run --name check-site -p 9889:80 my_nginx > /dev/null 2>&1 &
 sleep 5
+
+echo "Check curl status"
+resp_status=$(curl -I http:// 10.128.0.252:9889 >/dev/null | head -n 1 | cut -d$' ' -f2)
+echo "$resp_status"
+
 echo "Get md5sum index.html in container"
 index_docker=$(docker exec check-site md5sum /usr/share/nginx/html/index.html)
 index_docker=$(echo $index_docker | awk '{ print $1 }')
+
 echo "Get md5sum index.html in repo"
 index_repo=$(md5sum ./index.html | awk '{ print $1 }')
-echo $index_docker
-echo $index_repo
+
 echo "Remove docker"
 docker stop check-site > /dev/null
 docker rm check-site > /dev/null
 docker rmi my_nginx -f > /dev/null
+
+echo "Check index files sums"
+if [ $index_repo == $index_docker ]; then
+	echo "files match"
+else
+	echo "files not match"
+	exit 1
+fi
 
