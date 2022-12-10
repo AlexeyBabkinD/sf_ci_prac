c2aa75276514fd1e8f1f1357256bbc05fa0172c5
Alexey Babkin <alexey.babkin.d@gmail.com>
2022-12-10 20:34:33 +0300

    added jenkinsfile

---
+++ jenkins/check-site.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent {label 'docker'}
+    stages {
+        stage('check-site'){
+            steps {
+                script{
+                    sh 'docker build . -t my_nginx'
+                    sh 'docker run --name check-site -p 9889:80 my_nginx &'
+                    sh 'docker exec -it check-site md5sum /usr/share/nginx/html/index.html'
+                    sh 'docker stop check-site && docker rm check-site'
+                }
+            }
+        }
+    }
+}
